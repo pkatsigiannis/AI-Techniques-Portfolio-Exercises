971c1574dbbd507ef485f4a91812126308da1c7c
Panos Katsigiannis <pkatsigiannis@york.citycollege.eu>
2026-05-10 19:54:56 +0300

    Add files via upload

---
+++ Port3.py
@@ -0,0 +1,67 @@
+# Install library: 
+# pip install python-constraint
+
+from constraint import Problem, AllDifferentConstraint
+
+
+def office_next_to(a, b):
+    return abs(a - b) == 1
+
+
+def solve():
+    problem = Problem()
+
+    departments = ["CS", "Mathematics", "Philosophy", "History", "Dept5"]
+    research = ["AI", "ClimateChange", "QuantumPhysics", "Neuroscience", "MedievalLiterature"]
+    cars = ["Tesla", "BMW", "Mercedes", "Audi", "Volvo"]
+    universities = ["Oxford", "Cambridge", "Harvard", "MIT", "Stanford"]
+    decor = ["Blue", "Green", "Red", "Yellow", "White"]
+    drinks = ["Espresso", "HerbalTea", "GreenTea", "BlackCoffee", "Drink5"]
+
+    variables = departments + research + cars + universities + decor + drinks
+
+    problem.addVariables(variables, [1, 2, 3, 4, 5])
+
+    problem.addConstraint(AllDifferentConstraint(), departments)
+    problem.addConstraint(AllDifferentConstraint(), research)
+    problem.addConstraint(AllDifferentConstraint(), cars)
+    problem.addConstraint(AllDifferentConstraint(), universities)
+    problem.addConstraint(AllDifferentConstraint(), decor)
+    problem.addConstraint(AllDifferentConstraint(), drinks)
+
+    problem.addConstraint(lambda cs, blue: cs == blue, ["CS", "Blue"])
+    problem.addConstraint(lambda oxford, tesla: oxford == tesla, ["Oxford", "Tesla"])
+    problem.addConstraint(lambda ai, espresso: ai == espresso, ["AI", "Espresso"])
+    problem.addConstraint(lambda cambridge: cambridge == 1, ["Cambridge"])
+    problem.addConstraint(office_next_to, ["BMW", "Green"])
+    problem.addConstraint(lambda cc, tea: cc == tea, ["ClimateChange", "HerbalTea"])
+    problem.addConstraint(lambda math, red: math == red, ["Mathematics", "Red"])
+    problem.addConstraint(lambda mercedes, qp: mercedes == qp, ["Mercedes", "QuantumPhysics"])
+    problem.addConstraint(lambda green_tea: green_tea == 3, ["GreenTea"])
+    problem.addConstraint(office_next_to, ["Cambridge", "Yellow"])
+    problem.addConstraint(lambda volvo, philosophy: volvo == philosophy, ["Volvo", "Philosophy"])
+    problem.addConstraint(office_next_to, ["Neuroscience", "Audi"])
+    problem.addConstraint(lambda history, coffee: history == coffee, ["History", "BlackCoffee"])
+    problem.addConstraint(lambda white, mit: white == mit, ["White", "MIT"])
+    problem.addConstraint(lambda stanford, harvard: stanford > harvard, ["Stanford", "Harvard"])
+
+    solutions = problem.getSolutions()
+
+    possible_professors = set()
+
+    for solution in solutions:
+        medieval_office = solution["MedievalLiterature"]
+
+        for department in departments:
+            if solution[department] == medieval_office:
+                possible_professors.add(department)
+
+    print("Number of valid solutions:", len(solutions))
+    print("Possible professors researching Medieval Literature:")
+
+    for professor in sorted(possible_professors):
+        print("-", professor)
+
+
+if __name__ == "__main__":
+    solve()
